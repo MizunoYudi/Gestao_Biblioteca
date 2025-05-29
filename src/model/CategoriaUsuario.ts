f8bd78965aff80a207f55f1397a191f1487aacc0
MizunoYudi <joaomizunovy@gmail.com>
2025-05-29 20:33:56 -0300

    Definição da Camada Model

---
+++ src/model/CategoriaUsuario.ts
@@ -0,0 +1,12 @@
+export class CategoriaUsuario {
+    id: number
+    nome: string
+
+    constructor(
+        id: number,
+        nome: string
+    ){
+        this.id = id
+        this.nome = nome
+    }
+}
